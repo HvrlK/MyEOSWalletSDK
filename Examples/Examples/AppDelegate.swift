018e6799c3559f352a17a7f1f6b0e717ae203fb2
Alex Melnichuk <a.melnichuk@yahoo.com>
2018-11-25 18:29:58 +0200

    Bump up version

--- Examples/Examples/AppDelegate.swift
+++ Examples/Examples/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PaytomatSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        PaytomatSDK.setup(configuration: PaytomatSDK.Configuration())
+        PaytomatSDK.shared.test()
+        print(PaytomatSDK.shared.appInfo())
         return true
     }
 
--- PaytomatSDK.podspec
+++ PaytomatSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "PaytomatSDK"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.swift_version = '4.2'
   s.summary      = "Access EOS account information, send EOS transfers"
   s.description  = <<-DESC 
